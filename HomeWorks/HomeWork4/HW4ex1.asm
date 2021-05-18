4c8831d88c53b32f9f723595246d8ae5404593de
Avichai-Aziz <73444505+Avichai-Aziz@users.noreply.github.com>
2021-05-18 19:37:54 +0300

    Update HW4ex1.asm

--- HomeWorks/HomeWork4/HW4ex1.asm
+++ HomeWorks/HomeWork4/HW4ex1.asm
@@ -11,29 +11,29 @@
 
 .CODE
 	start:    
-	    mov  ax, @DATA
-	    mov  ds, ax
+	    	mov  	ax, @DATA
+	    	mov  	ds, ax
 	;----------------the code itself begins here------------------
             
-        ;getting input
-	    mov  ah, 0AH
-	    lea  dx, string
-		int  21H
+        	;getting input
+	    	mov  	ah, 0AH
+	    	lea  	dx, string
+		int  	21H
 		
-		mov  si, dx
-	    mov  cx, [si+1] ; si+1 is the real size of the input
-		mov	 ch, 0		; cause the ch is the value of the first char in the string
+		mov  	si, dx
+	    	mov  	cx, [si+1] ; si+1 is the real size of the input
+		mov	ch, 0		; cause the ch is the value of the first char in the string
 
 	    CHANGE:
-			mov	 al, [si+2]	; plus 2 to get the first char in the string
-	        cmp  al, 'a' 	; check if the char is lower then 'a'
-	        jl   NEXT_CHAR 	; if true, then jmp to the next char 'z'
-	        cmp  al, 'z' 	; check if the char is bigger then 'z'
-	        jg   NEXT_CHAR 	; if true, then jmp to the next chari) to get the upcase of this char
-	        sub  al, 32  	; else, sub 32 from the char (ascii) to get the upcase of this char
+		mov	al, [si+2]	; plus 2 to get the first char in the string
+	        cmp  	al, 'a' 	; check if the char is lower then 'a'
+	        jl   	NEXT_CHAR 	; if true, then jmp to the next char 'z'
+	        cmp  	al, 'z' 	; check if the char is bigger then 'z'
+	        jg   	NEXT_CHAR 	; if true, then jmp to the next chari) to get the upcase of this char
+	        sub  	al, 32  	; else, sub 32 from the char (ascii) to get the upcase of this char
 	        NEXT_CHAR:
-				mov  [si+2], al
-	            inc  si
+			mov  	[si+2], al
+			inc  	si
 	    loop CHANGE
 
 		; print new line
